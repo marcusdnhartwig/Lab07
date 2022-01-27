03c5e79355848d523fa1f8e0c0699df1bbbfa3ea
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-01-27 14:42:35 -0800

    proj.

---
+++ index.js
@@ -0,0 +1,17 @@
+// step 1. -- get ingredients, bread, cheese, veg. meat, sause
+
+// Decalare funcion
+// style guiide
+funtion makeasandwich (){// all the code
+// gather ingredients
+    let bread = 'grain';
+    let meat = 'pork';
+    let sauce = 'mayo & mustard';
+    let veg = 'onion';
+
+    return 'here is your sandwich on' + bread +  
+} 
+// =same thing= //
+let makeasandwich = funtion () {
+
+}
